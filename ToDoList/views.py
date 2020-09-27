81092525390a74b99e05c0caaac19cbf8b225127
Euhid Aman <aman.derax20@gmail.com>
2020-09-27 20:55:27 +0530

    used POST to show saving of data in database

--- ToDoList/views.py
+++ ToDoList/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+# video time ==> 6:47:00
 
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def home(request):
+    return render(request, 'ToDoList/index.html')
+
+@csrf_exempt
+def add_todo(request):
+    print(request.POST)
     return render(request, 'ToDoList/index.html')
