549a3df572e030e3668990c944217c106abc1f18
Euhid Aman <aman.derax20@gmail.com>
2020-09-28 21:24:55 +0530

    Made storing of objects permanent in the database.
    Also, looped through database items to show in index.html

--- ToDoList/views.py
+++ ToDoList/views.py
@@ -2,12 +2,21 @@
 
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from ToDoList.models import ToDo
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'ToDoList/index.html')
+    todo_items = ToDo.objects.all().order_by("-added_date")
+    return render(request, 'ToDoList/index.html', {"todo_items": todo_items})
+
 
 @csrf_exempt
 def add_todo(request):
-    print(request.POST)
-    return render(request, 'ToDoList/index.html')
+    current_date = timezone.now()
+    content = request.POST["content"]
+    created_obj = ToDo.objects.create(added_date=current_date, text=content)
+    length_of_todos = ToDo.objects.all().count()
+    return HttpResponseRedirect('/ToDoList/')
